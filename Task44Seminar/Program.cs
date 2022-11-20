2b5044bf950a104467fcb751bb215976d5d65af2
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-20 10:56:11 +0700

    add task44 seminar

---
+++ Task44Seminar/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task44 Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1
+// если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Сколько чисел Фибоначчи вывести на экран?");
+int size = int.Parse(Console.ReadLine());
+
+int[] fibs = new int[size];
+for (int i = 0; i < 2; i++)
+{
+    fibs[i] = i;
+}
+
+for (int j = 2; j < fibs.Length; j++)
+{
+    fibs[j] = fibs[j-1] + fibs[j-2];
+}
+
+Console.WriteLine($"Первые {size} чисел Фибоначчи: [ {String.Join(", ", fibs)} ]");
