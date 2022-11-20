0878fa50d2b586ae3918940b7c4b3b046803868b
Sergey.Istomin <Sergej.istomin.2017@inbox.ru>
2022-11-20 10:42:31 +0700

    add task42 seminar

---
+++ Task42Seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task42 напишите программу, которая будет преобразовывать десятичное число в двоичное
+
+string ToBin(int num)
+{
+    string result = "";
+    string temp = "";
+    while (num > 0)
+    {
+        temp = Convert.ToString(num%2);
+        result = temp + result;
+        num = num / 2;
+    }
+    return result;
+}
+Console.WriteLine(ToBin(2));
